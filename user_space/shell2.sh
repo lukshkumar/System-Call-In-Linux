3738650505d642825c81273a46c039d942523887
lukshkumar <38881879+lukshkumar@users.noreply.github.com>
2018-05-01 19:02:13 +0500

    Create shell2.sh

---
+++ user_space/shell2.sh
@@ -0,0 +1,20 @@
+
+
+xcowsay SJF Algorithm Systems call 
+
+sleep 1
+
+ echo "$(echo 'System call returned 0 -------------- Successful Return-------------' | tail -50 | toilet -f term -F border --gay)";
+
+sleep 2;
+
+ echo "$(echo '-------------- Your Desired Output-------------' | tail -50 | toilet -f term -F border --gay)";
+
+sleep 1;
+
+ echo "$(dmesg | tail -33 | toilet -f term -F border --gay)";
+
+
+sleep 5;
+
+echo 'Thank you' | figlet | lolcat
