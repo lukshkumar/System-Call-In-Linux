4af5db1df533ec3eeb7693fb68498a6d70afda24
aliakber366 <32928670+aliakber366@users.noreply.github.com>
2018-05-03 13:45:32 +0500

    Slight Updates

--- kernel_space/include/linux/syscalls.h
+++ kernel_space/include/linux/syscalls.h
@@ -1,2 +1,5 @@
 #Declaration of the system call.
+# Add this at the bottom of file
 asmlinkage long sys_sjf(void);
+
+# you can pass parameters too. For our program, there are no parameters, hence, void is used.
